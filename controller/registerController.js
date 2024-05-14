beb222ddb95ee626683e34b076a2d7d4535e06e1
crysdann <crysdann12@gmail.com>
2024-05-13 23:04:58 -0400

    registration error messages

--- controller/registerController.js
+++ controller/registerController.js
@@ -1,14 +1,34 @@
 const registrationModel = require("../models/UserRegistration");
 
 const getregisterController = (req, res) => {
-  res.render("register");
+  res.render("register", {
+    errors: {},
+  });
 };
 
 const createnewUserRegisterController = async (req, res) => {
+  const { regusername, regpassword, regconfirmpassword } = req.body;
+  let errors = {};
+
+  if (!regusername) {
+    errors.usernameerror = "Username is required";
+  }
+  if (!regpassword) {
+    errors.passworderror = "Password is required";
+  }
+  if (!regconfirmpassword) {
+    errors.confirmPassworderror = "Confirm Password is required";
+  }
+  if (regpassword !== regconfirmpassword) {
+    errors.passwordMismatch = "Passwords do not match";
+  }
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).render("register", { errors });
+  }
   try {
     const newUserRegistration = await registrationModel.create({
-      username: req.body.regusername,
-      password: req.body.regpassword,
+      username: regusername,
+      password: regpassword,
     });
     res.redirect("/index");
   } catch (err) {
